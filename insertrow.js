37cd20acb86516729b283991115ddb34a851bc1d
João <107438412+joaofmota@users.noreply.github.com>
2022-08-03 14:44:19 +0100

    Update insertrow.js

--- insertrow.js
+++ insertrow.js
@@ -2,6 +2,13 @@ let mysql  = require('mysql');
 let config = require('./config.js');
 let connection = mysql.createConnection(config);
 
+var conn = mysql.createConnection({
+host:"mext.mysql.database.azure.com", 
+user:"username", 
+password:"password", 
+database:"database", 
+port:3306});
+
 // insert statment
 let sql = `INSERT INTO Table(Coluna1,Coluna2)
            VALUES('Exemplo',true)`;
